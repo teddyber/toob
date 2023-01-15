ffeff8a12166dd96d85f3d11e3c01ac7d2f02cf3
CARLIER Bertrand <bertrand.carlier@wavestone.com>
2023-01-15 14:25:07 +0100

    icons, better buttons, notifications

--- pj.php
+++ pj.php
@@ -2,8 +2,6 @@
 $headers = array('Content-Type: ', 'Content-Length: ');
 $file = file_get_contents($_GET['url'], false, null, 0, 0);
 foreach ($http_response_header as $header) {
-    // echo $header . "\n";
-    // echo substr($header, 0, strlen('Content-Length: '))."\n";
     foreach ($headers as $key) {
         if (substr($header, 0, strlen($key)) == $key) {
             $r[$key] = substr($header, strlen($key));
